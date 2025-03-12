7555264558d58b7fe320090b8e71ad12fd418e39
Dmitriy Cherchenko <dchenk@users.noreply.github.com>
2025-03-12 16:27:08 -0500

    Change the ignored request token type

--- typescript/src/web/main.ts
+++ typescript/src/web/main.ts
@@ -3,5 +3,7 @@ export * from './contexts.ts';
 /**
  * This value should be passed to an AbortController's abort method when cancelling a request that is meant to be
  * ignored instead of surfacing to the error handling logic as an error.
+ *
+ * This is an Error object because, for requests managed by ApolloClient, an Error is expected for the reason value.
  */
-export const ignoredRequestCanceledToken = Symbol('Request canceled');
+export const ignoredRequestCanceledToken = new Error('Request canceled');
